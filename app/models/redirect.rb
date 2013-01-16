63f5763a659e98ca4cdaa84af33c44940a2b22db
Tristan <tristan.gomez@gmail.com>
2013-01-17 04:20:39 +0800

    refactor map reduce

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -14,34 +14,31 @@ class Redirect
   field :ua, type: String
   index ua:1
 
-  REDUCE = %Q{
-    function(key, values) {
-      var result = { count: 0 };
-      values.forEach(function(value) {
-        result.count += value.count;
-      });
-      return result;
-    }
-  }
-
   def self.latest_hits
-    map = %Q{
-      function() {
-        emit(this.rqst, { count: 1 });
-      }
-    }
-    map_reduce(map, REDUCE).out(inline: true).map do |result|
-      { url: result['_id'], count: result['value']['count'].to_i }
-    end
+    perform_map_reduce_on :rqst
   end
 
   def self.latest_referrers
-    map = %Q{
-      function() {
-        emit(this.refr, { count: 1 });
+    perform_map_reduce_on :refr
+  end
+
+  private
+
+  def self.map(field)
+    "function() { emit(this.#{field.to_s}, { count: 1 }); }"
+  end
+
+  def self.perform_map_reduce_on(field)
+    reduce = %Q{
+      function(key, values) {
+        var result = { count: 0 };
+        values.forEach(function(value) {
+          result.count += value.count;
+        });
+        return result;
       }
     }
-    map_reduce(map, REDUCE).out(inline: true).map do |result|
+    map_reduce(map(field), reduce).out(inline: true).map do |result|
       { url: result['_id'], count: result['value']['count'].to_i }
     end
   end
