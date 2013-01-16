ad5cb763a93830ebc2bcd15c42226899c1dfb285
Tristan <tristan.gomez@gmail.com>
2013-01-17 04:59:21 +0800

    add in dashboard concept

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,11 @@
 class AdminController < ApplicationController
-	def dashboard
-	end
+  def dashboard
+    @latest_hits = Redirect.latest_hits.each do |hash|
+      hash[:date] = Redirect.where(rqst: hash[:url]).order_by(:created_at => :desc).first.created_at
+    end
+    @latest_referrers = Redirect.latest_referrers.each do |hash|
+      hash[:date] = Redirect.where(refr: hash[:url]).order_by(:created_at => :desc).first.created_at
+      hash[:url] ||= '(direct)'
+    end
+  end
 end
