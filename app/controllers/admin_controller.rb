82e76a1357f70ddd6462d0cd99a09c4a23b5309b
Tristan <tristan.gomez@gmail.com>
2013-01-17 06:07:56 +0800

    sort by descending date

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,10 +2,10 @@ class AdminController < ApplicationController
   def dashboard
     @latest_hits = Redirect.latest_hits.each do |hash|
       hash[:date] = Redirect.where(rqst: hash[:url]).order_by(:created_at => :desc).first.created_at
-    end
+    end.sort {|x, y| y[:date] <=> x[:date]}
     @latest_referrers = Redirect.latest_referrers.each do |hash|
       hash[:date] = Redirect.where(refr: hash[:url]).order_by(:created_at => :desc).first.created_at
       hash[:url] ||= '(direct)'
-    end
+    end.sort {|x, y| y[:date] <=> x[:date]}
   end
 end
